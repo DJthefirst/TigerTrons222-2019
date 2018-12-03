c60dbd96e218b81840cc6a5d1a920734831daac2
Tigertrons <mr.andi321@gmail.com>
2018-12-03 13:24:56 -0500

    Drivetrain + RobotMap - talons // Andrzej

---
+++ FRC Default Code/src/org/usfirst/frc/team222/robot/subsystems/Drivetrain.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team222.robot;
+public class Drivetrain extends Subsystems {
+		
+
+
+public Drivetrain()
+{
+	Talon leftFrontTalon = Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
+	Talon leftFrontTalon = Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
+	Talon leftFrontTalon = Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
+	Talon leftFrontTalon = Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
+}
+
+}
