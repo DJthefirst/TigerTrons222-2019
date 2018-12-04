97ea77291f472378e5262a7d9413c2e6ed6aba95
AndrzejSwietek2001 <mr.andi321@gmail.com>
2018-12-04 18:47:58 -0500

    Andrzejek

--- FRC Default Code/src/org/usfirst/frc/team222/robot/commands/DriveArcade.java
+++ FRC Default Code/src/org/usfirst/frc/team222/robot/commands/DriveArcade.java
@@ -18,7 +18,8 @@ public class DriveArcade extends Command {
     protected void execute()
     {
         double moveSpeed = -Robot.m_oi.driveController.getRawAxis(RobotMap.DRIVER_CONTROLLER_MOVE_AXIS);
-        double moveSpeed = Robot.m_oi.driveController.getRawAxis(RobotMap.DRIVER_CONTROLLER_MOVE_AXIS);
+        double rotateSpeed = Robot.m_oi.driveController.getRawAxis(RobotMap.DRIVER_CONTROLLER_MOVE_AXIS);
+        Robot.m_drivetrain.arcadeDrive(moveSpeed,rotateSpeed);
     }
 
     @Override 
@@ -31,6 +32,7 @@ public class DriveArcade extends Command {
 
     @Override 
     protected void end(){       
+        
     }
 
     @Override 
