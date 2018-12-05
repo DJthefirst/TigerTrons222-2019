cd758bf4b5b7d5103dfa1e14ac73e69b53311fbe
David Tech <David.Tech@TASD.NET>
2018-12-05 13:06:37 -0500

    Motor test

--- FRC Default Code/src/org/usfirst/frc/team222/robot/subsystems/Drivetrain.java
+++ FRC Default Code/src/org/usfirst/frc/team222/robot/subsystems/Drivetrain.java
@@ -5,10 +5,10 @@ DifferentialDrive differentialDrive = null ;
 
 public Drivetrain()
 {
-	Talon leftFrontTalon = Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
-	Talon rightFrontTalon = Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
-	Talon leftBackTalon = Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
-	Talon rightBackTalon = Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
+	WPI_TalonSRX leftFrontTalon = WPI_TalonSRX(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
+	WPI_TalonSRX rightFrontTalon = WPI_TalonSRX(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
+	WPI_TalonSRX leftBackTalon = WPI_TalonSRX(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
+	WPI_TalonSRX rightBackTalon = WPI_TalonSRX(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
 
 	SpeedControllerGroup leftMotors = new SpeedControllerGroup(leftFrontTalon,leftBackTalon);
 	SpeedControllerGroup rightMotors = new SpeedControllerGoup(rightFrontTalon,rightBackTalon);
